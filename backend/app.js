f962a37b934e4e53ab915326c1512c82ba6dffa7
Ayoze <tu@email.com>
2025-01-25 12:52:31 +0000

    definiendo ruta basica para que funciona el deploy back

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(router);
 
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando");
+  });
+
 // Llama a la función para conectar a MongoDB
 connectDB();
 
