877fbe468cad25082d6decc91108c173ef65dc67
ayozegr05 <ayozegr05@gmail.com>
2025-02-25 00:33:01 +0700

    delete svg transformer and changing metro.config

--- Frontend/PickControl/metro.config.js
+++ Frontend/PickControl/metro.config.js
@@ -2,18 +2,5 @@ const { getDefaultConfig } = require("@expo/metro-config");
 
 module.exports = (async () => {
   const config = await getDefaultConfig(__dirname);
-
-  const { resolver: { sourceExts, assetExts } } = config;
-
-  return {
-    ...config, // Mantiene la configuración de Expo
-    transformer: {
-      ...config.transformer,
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"],
-    },
-  };
+  return config;
 })();
